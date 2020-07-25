a1a6d85b09e8e88f1b896011d66ca0c8d52e3450
Dmitry Bruhanov <dmitrii_bryuhanov@mail.ru>
2020-07-25 17:03:07 +0300

    added Mock Data

---
+++ Data/Servises/TripService.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+
+namespace Trips.Data
+{
+    class TripService : ITripService
+    {
+        public void AddTrip(Trip trip)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Deletetrip(int tripId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<Trip> GetAllTrips()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Trip GetTripById(int tripId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Updatetrip(int tripId, Trip trip)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
