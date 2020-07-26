805b0e500ba8e8ca8da408cc906a4003e50fc980
Dmitry Bruhanov <dmitrii_bryuhanov@mail.ru>
2020-07-26 10:32:42 +0300

    added Create Component

--- ClientApp/src/components/NavMenu.js
+++ ClientApp/src/components/NavMenu.js
@@ -26,12 +26,15 @@ export class NavMenu extends Component {
       <header>
         <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
           <Container>
-            <NavbarBrand tag={Link} to="/">Tips</NavbarBrand>
+            <NavbarBrand tag={Link} to="/">About</NavbarBrand>
             <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
             <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
               <ul className="navbar-nav flex-grow">
+              <NavItem>
+                  <NavLink tag={Link} className="text-dark" to="/create">Create</NavLink>
+                </NavItem>
                 <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/trips">Trips</NavLink>
+                  <NavLink tag={Link} className="text-dark" to="/trips">View Trips</NavLink>
                 </NavItem>
               </ul>
             </Collapse>
